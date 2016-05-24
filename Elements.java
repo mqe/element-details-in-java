7d4a5373af8d1d4fe215321db0450d878e91c66c
Michael Sjoeberg <mail@michaelsjoeberg.com>
2016-05-24 21:04:07 +0200

    re-structure and print all

--- Elements.java
+++ Elements.java
@@ -27,24 +27,7 @@ public class Elements
     */
   private void populateMap()
   { 
-    // hydrogen
-    // this.elementMap.put("H", new ArrayList<String>());
-    // this.elementMap.get("H").add("1");
-    // this.elementMap.get("H").add("Hydrogen");
-    // this.elementMap.get("H").add("1.008");
-
-    // // helium
-    // this.elementMap.put("He", new ArrayList<String>());
-    // this.elementMap.get("He").add("2");
-    // this.elementMap.get("He").add("Helium");
-    // this.elementMap.get("He").add("4.002602");
-
-    // // helium
-    // this.elementMap.put("Li", new ArrayList<String>());
-    // this.elementMap.get("Li").add("3");
-    // this.elementMap.get("Li").add("Lithium");
-    // this.elementMap.get("Li").add("6.94");
-
+    // reader
     String csvFile = "data.csv";
     BufferedReader br = null;
     String line = "";
@@ -60,10 +43,10 @@ public class Elements
         String[] elementData = line.split(",");
 
         // populate map with element data
-        this.elementMap.put(elementData[0], new ArrayList<String>());
-        this.elementMap.get(elementData[0]).add(elementData[1]);
-        this.elementMap.get(elementData[0]).add(elementData[2]);
-        this.elementMap.get(elementData[0]).add(elementData[3]);
+        this.elementMap.put(elementData[2], new ArrayList<String>());
+        this.elementMap.get(elementData[2]).add(elementData[1]);
+        this.elementMap.get(elementData[2]).add(elementData[0]);
+        this.elementMap.get(elementData[2]).add(elementData[3]);
       }
     }
 
@@ -92,115 +75,100 @@ public class Elements
       }
     }
   }
-   
+
   /**
     * Method for printing element data from map.
     * 
     * @param aKey - symbol of element to print
     * @param aFormat - format for table
-    * @param aString - string for error messages
+    * @param aString - string for 'not found'
     */
-  private void printMapValue(String aKey, String aFormat, StringBuilder aString)
+  private void printElementData(String aKey, String aFormat, StringBuilder aString)
   {
-    // check if map has key (element symbol)
-    if (this.elementMap.get(aKey) != null)
+    // print all data in map
+    if (aKey.equals("all") || aKey.equals("All"))
     {
-      // create temporary list for element data
-      List<String> tempList = this.elementMap.get(aKey);
-         
-      // print data for element using format
-      System.out.printf(aFormat, aKey, tempList.get(1), tempList.get(0), tempList.get(2));
+      // print data for all elements in map
+      for (Map.Entry<String, List<String>> element : elementMap.entrySet())
+      { 
+        // define map variables
+        String key = element.getKey();
+        List<String> tempList = element.getValue();
+
+        // print element data
+        System.out.printf(aFormat, key, tempList.get(0), tempList.get(1), tempList.get(2));
+      }
     }
     else
     {
-      // add to string
-      aString.append(aKey);
-      aString.append(" ");
+      // print specific element data
+      if (this.elementMap.get(aKey) != null)
+      {
+        // create temporary list for element data
+        List<String> tempList = this.elementMap.get(aKey);
+         
+        // print element data
+        System.out.printf(aFormat, aKey, tempList.get(1), tempList.get(0), tempList.get(2));
+      }
+      else
+      {
+        // add input key to 'not found' string
+        aString.append(aKey);
+        aString.append(" ");
+      }
     }
   }
    
   /**
-    * Method for adding a new entry to the map.
+    * Method for printing table.
     * 
-    * @param aKey - key as a String representation of Symbol for an element
-    * @param aValue - value as a String representation of properties for the element
+    * @param aMap - map with element data
+    * @param args[] - list of arguments from input
     */
-  public void addMapEntry(String aKey, String aValue)
+  private static void printElementTable(Elements aMap, String args[])
   {
-    // split string into values for list
-    String[] newValues;
-    newValues = aValue.split(" ");
-      
-    // check if map has key
-    if (this.elementMap.get(aKey) == null)
-    {    
-      // create key and new list for element
-      this.elementMap.put(aKey, new ArrayList<String>());
-         
-      // add values to list
-      this.elementMap.get(aKey).add(newValues[0]);
-      this.elementMap.get(aKey).add(newValues[1]);
-      this.elementMap.get(aKey).add(newValues[2]);
-    }
-    else
-    {
-      // get and overwrite an existing element
-      this.elementMap.get(aKey).set(0, newValues[0]);
-      this.elementMap.get(aKey).set(1, newValues[1]);
-      this.elementMap.get(aKey).set(2, newValues[2]);
-    }
-  }
-   
-  /**
-    * Method for deleting an entry in map.
-    * 
-    * @param aKey - key as a String representation of Symbol for an element
-    */
-  public boolean deleteEntry(String aKey)
-  {
-    // check if map has key
-    if (this.elementMap.get(aKey) == null)
-    {    
-      // return false
-      return false;
-    }
-    else
-    {
-      // remove entry from map and return true
-      this.elementMap.remove(aKey);
-      return true;
-    }
-  }
-
-  /* main */
-  public static void main(String args[])
-  {
-    // create and populate map
-    Elements aMap = new Elements();
-    aMap.populateMap();
-
+    // create new StringBuilder for 'not found' string
     StringBuilder notFound = new StringBuilder("Elements not found: ");
 
-    // variables
+    // format and separator for print
     String format = "%-15s%-15s%-15s%-15s\n";
     String separator = new String(new char[60]).replace('\0', '-');
 
-    // print the header for table
+    // print the table header
     System.out.println("");
     System.out.printf(format, "Symbol", "Name", "Number", "Weight");
     System.out.println(separator);
 
-    // print map values
-    for (String s: args) {
-      aMap.printMapValue(s, format, notFound);
+    // print data from map
+    for (String input: args) {
+      aMap.printElementData(input, format, notFound);
     }
 
-    // print the footer for table
+    // convert StringBuilder to string
+    String elementsNotFound = notFound.toString();
+
+    // print the table footer
     System.out.println("");
-    System.out.println(notFound.toString());
-    System.out.println("");
+    if (!elementsNotFound.equals("Elements not found: "))
+    {
+      System.out.println(elementsNotFound);
+      System.out.println("");
+    }
     System.out.println(separator);
     System.out.println("Powered by github.com/mqe");
     System.out.println("");
+  }
+
+  /* init */
+  public static void main(String args[])
+  {
+    // create new map object
+    Elements aMap = new Elements();
+
+    // populate map with data
+    aMap.populateMap();
+
+    // print table
+    printElementTable(aMap, args);
   }
 }
